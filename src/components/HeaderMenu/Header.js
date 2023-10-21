da5100a271841b15ffc88de9ea3ff40a6a731d1f
Aminul Kibria <99344233+dksifatofficial@users.noreply.github.com>
2023-10-21 15:20:02 +0600

    header

---
+++ src/components/HeaderMenu/Header.js
@@ -0,0 +1,9 @@
+const HeaderMenu = () => {
+  return (
+    <div className=" bg-slate-500 flex flex-row justify-around py-3 px-4 shadow-md">
+      Header
+    </div>
+  );
+};
+
+export default HeaderMenu;
