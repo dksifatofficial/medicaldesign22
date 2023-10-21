676a7f39132c4c97ff0a667c90f8eb8d95cdbf1e
Aminul Kibria <99344233+dksifatofficial@users.noreply.github.com>
2023-10-21 15:38:52 +0600

    layout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "images.unsplash.com",
+      "firebasestorage.googleapis.com",
+      "mongodb+srv://dksifat:123456782023@cluster0.j5selhy.mongodb.net",
+    ],
+  },
+};
